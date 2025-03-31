c84e06dca82cc79dc54cfb2ba8f223be458c4ec3
anuragbansode <anuragbansode10@gmail.com>
2025-03-31 12:26:59 +0530

    Academic Holidays Created

---
+++ src/app/holidays/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Typography, Box, Grid } from "@mui/material";
+import EventIcon from "@mui/icons-material/Event";
+import styles from "./holiday.module.css";
+import nextConfig from "../../../next.config";
+
+interface CalendarItem {
+  title: string;
+  url: string;
+}
+
+const Holidays: React.FC = () => {
+  const [calendar, setCalendar] = useState<CalendarItem[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    document.title = "Holidays";
+    return () => {
+      document.title = "IIIT Trichy";
+    };
+  }, []);
+
+  useEffect(() => {
+    fetch("/json/general/holidays.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch calendar data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCalendar(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching calendar data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="page-container">
+      <Grid container className={styles.container}>
+        <Grid xs={1} />
+        <Grid xs={10}>
+          <Typography
+            variant="h2"
+            component="h2"
+            gutterBottom
+            className={styles.themeText}
+          >
+            <Box component="span" fontWeight={380}>
+              Academic Calendar
+            </Box>
+          </Typography>
+          {calendar &&
+            calendar.map((item, id) => (
+              <section key={id} className={styles.sectionPadding}>
+                <Box display="flex" alignItems="center">
+                  <EventIcon className={styles.icon} />
+                  <a
+                    href={`${nextConfig.env?.DOCUMENT}/${item.url}`}
+                    download={`${item.title}`}
+                    className={styles.link}
+                  >
+                    {item.title}
+                  </a>
+                </Box>
+              </section>
+            ))}
+        </Grid>
+        <Grid xs={1} />
+      </Grid>
+    </div>
+  );
+};
+
+export default Holidays;
