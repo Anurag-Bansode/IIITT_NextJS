89ac0b6e799c4a99ac967a71cb231abbb2f5dde0
anuragbansode <anuragbansode10@gmail.com>
2025-04-28 21:36:49 +0530

    nav bar mobile view adjustments with logo

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -20,6 +20,21 @@ import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import PeopleIcon from '@mui/icons-material/People';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import PersonIcon from '@mui/icons-material/Person';
+import SchoolIcon from '@mui/icons-material/School';
+import CallIcon from '@mui/icons-material/Call';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import WorkIcon from '@mui/icons-material/Work';
+import FlagIcon from '@mui/icons-material/Flag';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import CodeIcon from '@mui/icons-material/Code';
 import Image from "next/image";
 import { styled } from "@mui/material/styles";
 import nextConfig from "../../../next.config";
@@ -29,12 +44,27 @@ interface SubMenuItem {
   text: string;
   link: string;
   submenu?: SubMenuItem[];
+  icon?: string;
 }
+const iconMap: { [key: string]: React.ComponentType } = {
+  account_balance: AccountBalanceIcon,
+  local_library: LocalLibraryIcon,
+  people: PeopleIcon,
+  note_add: NoteAddIcon,
+  notification_important: NotificationsIcon,
+  person: PersonIcon,
+  school: SchoolIcon,
+  call: CallIcon,
+  workspace_premium: WorkspacePremiumIcon,
+  work: WorkIcon,
+  code: CodeIcon,
+};
 
 interface NavItem {
   text: string;
   link?: string;
   submenu?: SubMenuItem[];
+  icon?: string;
 }
 
 const StyledHomeIcon = styled(HomeRoundedIcon)({
@@ -102,29 +132,33 @@ const Navbar = () => {
 
       {/* Sidebar Drawer */}
       <Drawer anchor="left" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
-        <List>
-          <ListItem>
-            <ListItemIcon>
-              <Image
-                src={`${nextConfig.env?.IMAGE}/iiitt-logo.png`}
-                alt="IIITT Logo"
-                priority
-                width={40}
-                height={80}
-              />
-            </ListItemIcon>
-            <ListItemText>
-              <Typography variant="h6" sx={{ color: "Black" }}>IIITT</Typography>
-            </ListItemText>
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          {navItems.map((menuItem, index) => (
-            <DropdownMenu key={index} menu={menuItem} />
-          ))}
-        </List>
-      </Drawer>
+  <List>
+    <ListItem>
+      <ListItemIcon>
+        <Image
+          src={`${nextConfig.env?.IMAGE}/iiitt-logo.png`}
+          alt="IIITT Logo"
+          priority
+          width={40}
+          height={80}
+        />
+      </ListItemIcon>
+      <ListItemText>
+        <Typography variant="h6" sx={{ color: "Black" }}>IIITT</Typography>
+      </ListItemText>
+    </ListItem>
+  </List>
+  <Divider />
+  <List>
+    {navItems.map((menuItem, index) => (
+      <MobileMenuItem
+        key={index}
+        item={menuItem}
+        onClose={() => setDrawerOpen(false)}
+      />
+    ))}
+  </List>
+</Drawer>
     </>
   );
 };
@@ -208,4 +242,51 @@ const NestedDropdown: React.FC<{ menuItem: SubMenuItem; onClose: () => void }> =
   );
 };
 
+const MobileMenuItem = ({ item, depth = 0, onClose }: { item: NavItem | SubMenuItem; depth?: number; onClose: () => void }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const hasSubmenu = item.submenu && item.submenu.length > 0;
+  const IconComponent = item.icon ? iconMap[item.icon] : null;
+
+  return (
+    <>
+      {!hasSubmenu ? (
+        <Link href={item.link || "#"} >
+          <ListItem
+
+            onClick={onClose}  // Close drawer when clicking a link
+            sx={{ pl: depth * 2 }}
+          >
+            {IconComponent && (
+              <ListItemIcon>
+                <IconComponent />
+              </ListItemIcon>
+            )}
+            <ListItemText primary={item.text} />
+          </ListItem>
+        </Link>
+      ) : (
+        <ListItem
+          onClick={() => setIsExpanded(!isExpanded)}
+          sx={{ pl: depth * 2 }}
+        >
+          {IconComponent && (
+            <ListItemIcon>
+              <IconComponent />
+            </ListItemIcon>
+          )}
+          <ListItemText primary={item.text} />
+          {isExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        </ListItem>
+      )}
+      {hasSubmenu && isExpanded && item.submenu?.map((subItem, index) => (
+        <MobileMenuItem
+          key={index}
+          item={subItem}
+          depth={depth + 1}
+          onClose={onClose}
+        />
+      ))}
+    </>
+  );
+};
 export default Navbar;
